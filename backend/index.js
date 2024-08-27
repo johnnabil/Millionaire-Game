1701826a2f0f40c0ff4da14dec5d139e448cb62e
johnnabil <john.nabil10@yahoo.com>
2024-08-27 18:24:12 +0300

    initial commit

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,11 @@ import cors from 'cors';
 config();
 const app = express();
 app.use(express.json());
-app.use(cors()); 
+app.use(cors({
+    origin: 'https://millionaire-game-odbs7pukc-johnnabil1s-projects.vercel.app', // Replace with your frontend URL
+    methods: 'GET,POST,PUT,DELETE',
+    credentials: true
+})); 
 app.use('/questions',questionsRoute);
 app.use('/scores', scoresRoute);
 
